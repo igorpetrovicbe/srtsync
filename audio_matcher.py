301b1ba581a1cfc9cabe8a2e9ddb55c52157cd0a
igorpetrovicbe <igor.be.petrovic@gmail.com>
2023-02-22 17:45:56 +0100

    feat: improved accuracy

--- audio_matcher.py
+++ audio_matcher.py
@@ -5,14 +5,19 @@ import whisper
 from fuzzywuzzy import fuzz
 
 
-def get_audio_timestamps(audio, subs, second_mode, offset, skip_percent=3 / 4):
-    print(torch.cuda.is_available())
+def get_audio_timestamps(audio, subs, second_mode, offset=0, skip_percent=0.5, model_type="tiny.en", min_ratio=80,
+                         minimum_length=30):
+    debug_mode = False
+
+    if debug_mode:
+        print(torch.cuda.is_available())
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    print(device)
-    model = whisper.load_model("tiny").to(device)
-    video_path = os.path.join(os.path.dirname(__file__), "hg.mp4")
+    if debug_mode:
+        print("Using device: " + device)
 
+    model = whisper.load_model(model_type).to(device)
     sub_sentences = []
 
     sentence = ""
@@ -24,7 +29,8 @@ def get_audio_timestamps(audio, subs, second_mode, offset, skip_percent=3 / 4):
                 sentence = sentence.replace('\n', ' ')
                 end_time = (sub.end - sub.start) * i / len(sub.content) + sub.start
                 sub_sentences.append((sentence[:], end_time.total_seconds()))
-                print(sentence)
+                if False and debug_mode:
+                    print(sentence)
                 sentence = ""
                 while i + 1 < len(sub.content):
                     if sub.content[i + 1] == '.' or sub.content[i + 1] == '?' or sub.content[i + 1] == '!':
@@ -35,8 +41,9 @@ def get_audio_timestamps(audio, subs, second_mode, offset, skip_percent=3 / 4):
                 sentence += char
             i += 1
 
-    result = model.transcribe(audio)
-    print(result)
+    result = model.transcribe(audio, language="en", max_initial_timestamp=None)
+    if debug_mode:
+        print(result)
     segments = result["segments"]
 
     transcript_sentences = []
@@ -51,7 +58,8 @@ def get_audio_timestamps(audio, subs, second_mode, offset, skip_percent=3 / 4):
                 sentence = sentence.replace('\n', ' ')
                 end_time = (segment["end"] - segment["start"]) * i / len(text) + segment["start"] + offset
                 transcript_sentences.append((sentence[:], end_time))
-                print(sentence)
+                if debug_mode:
+                    print(sentence)
                 sentence = ""
                 while i + 1 < len(text):
                     if segment["text"][i + 1] == '.' or segment["text"][i + 1] == '?' or segment["text"][i + 1] == '!':
@@ -63,15 +71,16 @@ def get_audio_timestamps(audio, subs, second_mode, offset, skip_percent=3 / 4):
             i += 1
 
     start = 0
-    last = start
-    minimum_length = 20
-    max_span = 40
-    min_ratio = 70
-    skipped = 0
+    max_span = 200
     sub_times = []
-    start_offset = len(sub_sentences) * skip_percent
+    if second_mode:
+        start_offset_sub = int(len(sub_sentences) * skip_percent)  # Not in use for now
+    else:
+        start_offset_sub = 0
+    # start_offset_trans = int(len(transcript_sentences) * skip_percent)  # Not in use for now
+    last = 0
     transcript_times = []
-    for s in range(len(sub_sentences)):
+    for s in range(start_offset_sub, len(sub_sentences)):
         sub_sentence = sub_sentences[s]
         if len(sub_sentence[0]) < minimum_length:
             if (start != 0 or not second_mode) and last < len(transcript_sentences) and len(transcript_sentences[last][0]) < minimum_length:
@@ -82,7 +91,8 @@ def get_audio_timestamps(audio, subs, second_mode, offset, skip_percent=3 / 4):
             transcript_sentence = transcript_sentences[i]
             ratio = fuzz.ratio(sub_sentence[0], transcript_sentence[0])
             if ratio > min_ratio:
-                print(sub_sentence[0] + " <-- " + transcript_sentence[0])
+                if debug_mode:
+                    print(sub_sentence[0] + " <-- " + transcript_sentence[0])
                 sub_times.append(sub_sentence[1])
                 transcript_times.append(transcript_sentence[1])
                 last = i
